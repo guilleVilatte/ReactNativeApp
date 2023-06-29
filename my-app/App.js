4c721d1f29274d239c02d961949f2caff6c0c49a
Guille <guillevilatte@gmail.com>
2023-06-29 15:55:13 +0200

    wip

--- my-app/App.js
+++ my-app/App.js
@@ -1,10 +1,14 @@
 import {
-  View, TextInput, Button, SafeAreaView,
+  View, TextInput, Button, SafeAreaView, Text, FlatList, Modal, TouchableOpacity
 } from 'react-native';
 import { styles } from './styles';
 import { useState } from 'react';
 
 export default function App() {
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
+  const [isVisible, setIsVisible] = useState(false)
+  const [selectedTask, setSelectedTask] = useState(null)
   const [borderColor, setBorderColor] = useState('#424D9E')
 
   const handleFocus = () => {
@@ -13,6 +17,43 @@ export default function App() {
 
   const handleBlur = () => {
     setBorderColor('#C5C9E7')
+  }
+
+  const handleTask = (event) => {
+    setTask(event)
+  }
+
+  const handleNewTask = () => {
+    setTasks([
+      ...tasks,
+      {
+        id: Date.now().toString(),
+        value: task
+      }
+    ])
+    setTask('')
+  }
+
+  const handleModal = (item) => {
+    setIsVisible(true)
+    setSelectedTask(item)
+  }
+
+  const handleRenderItem = ({item}) => (
+    <TouchableOpacity style={styles.taskContainer} onPress={() => handleModal(item)}>
+      <Text style={styles.task}>{item.value}</Text>
+      <Text style={styles.delete}>X</Text>
+    </TouchableOpacity>
+  )
+
+  const handleDeleteTask = () => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== selectedTask.id))
+    setIsVisible(false)
+  }
+
+  const handleCloseModal = () => {
+    setIsVisible(false)
+    setSelectedTask(null)
   }
 
   return (
@@ -26,10 +67,43 @@ export default function App() {
             autoCorrect={false}
             onFocus={handleFocus}
             onBlur={handleBlur}
+            onChangeText={handleTask}
+            value={task}
           />
-          <Button title='Create' color='#424D9E'/>
+          <Button
+            title='Create'
+            color='#424D9E'
+            onPress={handleNewTask}
+            disabled={!task.length}
+          />
         </View>
+        <FlatList
+          style={styles.taskListContainer}
+          data={tasks}
+          renderItem={handleRenderItem}
+          keyExtractor={item => item.id}
+        />
       </View>
+      <Modal visible={isVisible} animationType='slide'>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Task detail</Text>
+          <View style={styles.modalBody}>
+            <Text>Are you sure to delete {selectedTask?.value}?</Text>
+          </View>
+          <View style={styles.modalButtons}>
+            <Button
+              title='Cancel'
+              color='#424D9E'
+              onPress={handleCloseModal}
+            />
+            <Button
+              title='Delete'
+              color='red'
+              onPress={handleDeleteTask}
+            />
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 }
--- my-app/styles.js
+++ my-app/styles.js
@@ -21,4 +21,50 @@ export const styles = StyleSheet.create({
       color: '#212121',
       padding: 3,
     },
+    taskListContainer: {
+      marginTop: 25,
+    },
+    taskContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingVertical: 10,
+      backgroundColor: '#D4D7ED',
+      borderRadius: 10,
+      shadowColor: "#000",
+      shadowOffset: {
+      	width: 0,
+      	height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+      marginBottom: 10,
+    },
+    task: {
+      fontWeight: 'bold',
+      padding: 10,
+      width: '90%',
+    },
+    delete: {
+      fontWeight: 'bold',
+      padding: 10,
+      color: 'red'
+    },
+    modalContainer: {
+      flex: 1,
+      marginTop: 50,
+      alignItems: 'center',
+      gap: 15,
+    },
+    modalTitle: {
+      fontWeight: 'bold',
+      marginBottom: 10,
+    },
+    modalBody: {
+      paddingHorizontal: 10,
+    },
+    modalButtons: {
+      flexDirection: 'row',
+      gap: 50,
+    }
   });
