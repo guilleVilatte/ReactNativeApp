46e396adb52f334788ca7879e44a92c4900fddc1
Guille <guillevilatte@gmail.com>
2023-07-06 11:27:14 +0200

    wip

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,106 +1,101 @@
-import {
-  View, TextInput, Button, SafeAreaView, Text, FlatList, Modal, TouchableOpacity
-} from 'react-native';
-import { styles } from './styles';
 import { useState } from 'react';
+import {
+  View,
+  TextInput,
+  Button,
+  SafeAreaView,
+  Text,
+  FlatList,
+  Modal,
+  TouchableOpacity,
+} from 'react-native';
+
+import { styles } from './styles';
 
 export default function App() {
-  const [task, setTask] = useState('')
-  const [tasks, setTasks] = useState([])
-  const [isVisible, setIsVisible] = useState(false)
-  const [selectedTask, setSelectedTask] = useState(null)
-  const [borderColor, setBorderColor] = useState('#424D9E')
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [isVisible, setIsVisible] = useState(false);
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [borderColor, setBorderColor] = useState('#424D9E');
 
   const handleFocus = () => {
-    setBorderColor('#424D9E')
-  }
+    setBorderColor('#424D9E');
+  };
 
   const handleBlur = () => {
-    setBorderColor('#C5C9E7')
-  }
+    setBorderColor('#C5C9E7');
+  };
 
   const handleTask = (event) => {
-    setTask(event)
-  }
+    setTask(event);
+  };
 
   const handleNewTask = () => {
     setTasks([
       ...tasks,
       {
         id: Date.now().toString(),
-        value: task
-      }
-    ])
-    setTask('')
-  }
+        value: task,
+      },
+    ]);
+    setTask('');
+  };
 
   const handleModal = (item) => {
-    setIsVisible(true)
-    setSelectedTask(item)
-  }
+    setIsVisible(true);
+    setSelectedTask(item);
+  };
 
-  const handleRenderItem = ({item}) => (
+  const handleRenderItem = ({ item }) => (
     <TouchableOpacity style={styles.taskContainer} onPress={() => handleModal(item)}>
       <Text style={styles.task}>{item.value}</Text>
       <Text style={styles.delete}>X</Text>
     </TouchableOpacity>
-  )
+  );
 
   const handleDeleteTask = () => {
-    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== selectedTask.id))
-    setIsVisible(false)
-  }
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== selectedTask.id));
+    setIsVisible(false);
+  };
 
   const handleCloseModal = () => {
-    setIsVisible(false)
-    setSelectedTask(null)
-  }
+    setIsVisible(false);
+    setSelectedTask(null);
+  };
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <View style={styles.inputContainer}>
           <TextInput
-            style={[styles.input, {borderColor: borderColor}]}
-            placeholder='New task'
-            placeholderTextColor='#C5C9E7'
+            style={[styles.input, { borderColor }]}
+            placeholder="New task"
+            placeholderTextColor="#C5C9E7"
             autoCorrect={false}
             onFocus={handleFocus}
             onBlur={handleBlur}
             onChangeText={handleTask}
             value={task}
           />
-          <Button
-            title='Create'
-            color='#424D9E'
-            onPress={handleNewTask}
-            disabled={!task.length}
-          />
+          <Button title="Create" color="#424D9E" onPress={handleNewTask} disabled={!task.length} />
         </View>
         <FlatList
           style={styles.taskListContainer}
           data={tasks}
           renderItem={handleRenderItem}
-          keyExtractor={item => item.id}
+          keyExtractor={(item) => item.id}
         />
       </View>
-      <Modal visible={isVisible} animationType='slide'>
+      <Modal visible={isVisible} animationType="slide">
         <View style={styles.modalContainer}>
           <Text style={styles.modalTitle}>Task detail</Text>
           <View style={styles.modalBody}>
             <Text>Are you sure to delete {selectedTask?.value}?</Text>
           </View>
           <View style={styles.modalButtons}>
-            <Button
-              title='Cancel'
-              color='#424D9E'
-              onPress={handleCloseModal}
-            />
-            <Button
-              title='Delete'
-              color='red'
-              onPress={handleDeleteTask}
-            />
+            <Button title="Cancel" color="#424D9E" onPress={handleCloseModal} />
+            <Button title="Delete" color="red" onPress={handleDeleteTask} />
           </View>
         </View>
       </Modal>
---
+++ my-app/src/styles.js
@@ -0,0 +1,70 @@
+import { StyleSheet, StatusBar } from 'react-native';
+
+export const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 10,
+  },
+  inputContainer: {
+    paddingTop: StatusBar.currentHeight,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  input: {
+    flex: 0.95,
+    borderWidth: 0.7,
+    borderRadius: 8,
+    color: '#212121',
+    padding: 3,
+  },
+  taskListContainer: {
+    marginTop: 25,
+  },
+  taskContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    backgroundColor: '#D4D7ED',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    marginBottom: 10,
+  },
+  task: {
+    fontWeight: 'bold',
+    padding: 10,
+    width: '90%',
+  },
+  delete: {
+    fontWeight: 'bold',
+    padding: 10,
+    color: 'red',
+  },
+  modalContainer: {
+    flex: 1,
+    marginTop: 50,
+    alignItems: 'center',
+    gap: 15,
+  },
+  modalTitle: {
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  modalBody: {
+    paddingHorizontal: 10,
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    gap: 50,
+  },
+});
